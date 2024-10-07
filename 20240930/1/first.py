bcb02d8c3f3c61730601f3b2313d670fc45546c9
Ksenia Voroshilova <ksenia.sevastopol5@gmail.com>
2024-10-07 22:51:31 +0300

    .

--- 20240930/1/first.py
+++ 20240930/1/first.py
@@ -1,2 +1,2 @@
-m, n = eval(input())
-print([i for i in range(m, n) if not any(i % j == 0 for j in range(2, int(i ** 0.5) + 1)) and i != 1])
+a, b = eval(input())
+print([i for i in range(a, b) if not any(i % j == 0 for j in range(2, int(i ** 0.5) + 1)) and i != 1])
