47eb51b49f70295a9181185db1422f247e87a485
Ksenia Voroshilova <ksenia.sevastopol5@gmail.com>
2024-10-07 22:59:41 +0300

    edited

--- 20240923/1/third.py
+++ 20240923/1/third.py
@@ -1,21 +1,15 @@
+
 N = int(input())
-i = N
-while i <= N + 2:
-    j = N
-    while j <= N + 2:
-        col = i * j
-        r = 0
-        tmp_col = col
-        while tmp_col > 0:
-            r += tmp_col % 10
-            tmp_col //= 10
+for i in range(N, N + 3):
+    for j in range(N, N + 3):
+        product = i * j
+        digit_sum = sum(int(digit) for digit in str(product))
 
-        if r == 6:
-            result = ":=)"
+        if digit_sum == 6:
+            output = ":=)"
         else:
-            result = str(col)
+            output = str(product)
 
-        print(f"{i} * {j} = {result:<5}", end=' ')
-        j += 1
+        print(f"{i} * {j} = {output:<5}", end=' ')
     print()
-    i += 1
+
